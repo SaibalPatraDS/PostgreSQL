dc78cd8a6cd4430a480092311be0fc3eaae02f35
Saibal Patra <102281722+SaibalPatraDS@users.noreply.github.com>
2023-07-09 19:08:12 +0530

    Update solution2questions.sql

--- Practice SQL /solution2questions.sql
+++ Practice SQL /solution2questions.sql
@@ -60,5 +60,11 @@ ORDER BY weight_group DESC;
 */
 
 -- Solutions - 
-
+SELECT patient_id,
+       weight,
+       height,
+       CASE WHEN weight/POWER(height/100.0, 2) >= 30 THEN 1
+            ELSE 0
+       END AS isObese
+FROM patients;
 
